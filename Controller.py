51ea9e6e4d0f7fc244ae7d4327e80015aa5b1f22
legacysistemas <contato@legacysistemas.com>
2025-05-08 20:04:13 -0300

    FINALIZADA AULA 10 - REMOVER CATEGORIA (CONTROLLER) - ALTERAR CATEGORIA (CONTROLLER) - MOSTRAR CATEGORIA (CONTROLLER) - CADASTRO DE PRODUTO (CONTROLLER) - REMOVER PRODUTO (CONTROLLER)

--- Controller.py
+++ Controller.py
@@ -15,5 +15,83 @@ class ControllerCategoria:
         else:
             print('Categoria já cadastrada!')
 
-a = ControllerCategoria()
-a.cadastrarCategoria('teste')
+    def removerCategoria(self, categoriaRemover):
+        x = DaoCategoria.ler()
+        cat = list (filter(lambda x: x.categoria == categoriaRemover, x))
+
+        if len(cat) <= 0:
+            print('Categoria não cadastrada!')
+        else:
+            for i in range(len(x)):
+                if x[i].categoria == categoriaRemover:
+                    del x[i]
+                    break
+            print('Categoria removida com sucesso!')
+
+            with open('categorias.txt', 'w') as arq:
+                for i in x:
+                    arq.writelines(i.categoria)
+                    arq.writelines('\n')
+
+    def alterarCategoria(self, categoriaAlterar, categoriaAlterada):
+        x = DaoCategoria.ler()
+        cat = list(filter(lambda x: x.categoria == categoriaAlterar, x))
+
+        if len(cat) > 0:
+            cat1 = list(filter(lambda x: x.categoria == categoriaAlterada, x))
+            if len(cat1) == 0:
+                x = list(map(lambda x: Categoria(categoriaAlterada) if (x.categoria == categoriaAlterar) else (x), x))
+                print('Categoria alterada com sucesso!')
+
+            else:
+                print('Categoria já cadastrada!')
+        else:
+            print('Categoria nao cadastrada!')
+
+        with open('categorias.txt', 'w') as arq:
+            for i in x:
+                arq.writelines(i.categoria)
+                arq.writelines('\n')
+
+    def mostrarCategoria(self):
+        categorias = DaoCategoria.ler()
+        if len(categorias) == 0:
+            print('Nenhuma categoria cadastrada!')
+        else:
+            for i in categorias:
+                print(f'Categoria: {i.categoria}')
+
+class ControllerEstoque:
+    def cadastrarProduto(self, nome, preco, categoria, quantidade):
+        x = DaoEstoque.ler()
+        y = DaoCategoria.ler()
+        h = list(filter(lambda x: x.categoria == categoria, y))
+        est = list(filter(lambda x: x.produto.nome == nome, x))
+
+        if len (h) > 0:
+            if len(est) == 0:
+                produto = Produtos(nome, preco, categoria)
+                DaoEstoque.salvar(produto, quantidade)
+                print('Produto cadastrado com sucesso!')
+            else:
+                print('Produto ja cadastrado!')
+        else:
+            print('Categoria nao cadastrada!')
+    
+    def removerProduto(self, nome):
+        x = DaoEstoque.ler()
+        est = list(filter(lambda x: x.produto.nome == nome, x))
+
+        if len(est) > 0:
+            for i in range(len(x)):
+                if x[i].produto.nome == nome:
+                    del x[i]
+                    break
+            print('Produto removido com sucesso!')
+        else:
+            print('Produto nao cadastrado!')
+
+        with open('estoque.txt', 'w') as arq:
+                for i in x:
+                    arq.writelines(i.produto.nome + "|" + i.produto.preco + "|" + i.produto.categoria + "|" + str(i.quantidade))
+                    arq.writelines('\n')
